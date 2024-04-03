def469c949e7e3bb0ef57d3a635f3f44fd40da87
Nikolya <75427338+korhenon@users.noreply.github.com>
2024-04-03 11:07:21 +0300

    Создан метод на получение всех доступных валют

--- routers/rates.py
+++ routers/rates.py
@@ -1,15 +1,35 @@
 from fastapi import APIRouter
 
-from controllers.rates import RatesController
+from controllers.rates import RatesController, codes
 
 router = APIRouter(prefix="/rates")
 
 
-@router.get("/currency_for_date/{date}")
-def get_currency_for_date_in_iso_format(date: str):
+@router.get("/available_currencies")
+def get_available_currencies():
+    return codes.keys()
+
+
+@router.get("/currencies/{date}")
+def get_currencies_for_date(date: str):
     return RatesController.get_currencies(date)
 
 
-@router.get("/currency_for_date/{currency}/{from_date}/{to_date}")
-def get_currency_for_date_in_iso_format(currency: str, from_date: str, to_date: str):
+@router.get("/currency/{currency}/{from_date}/{to_date}")
+def get_currency_for_period(currency: str, from_date: str, to_date: str):
     return RatesController.get_currency_for_period(currency, from_date, to_date)
+
+
+@router.route('/available_metals')
+def get_available_metals():
+    return ['Au', 'Ag', 'Pt', 'Pd']
+
+
+@router.get("/metals/{date}")
+def get_metals_for_date(date: str):
+    return
+
+
+@router.get("/metal/{metal}/{from_date}/{to_date}")
+def get_metals_for_date(metal: str, from_date: str, to_date: str):
+    return
