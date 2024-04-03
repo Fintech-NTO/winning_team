bb9a1aace0947c99d2f3760dee9dff37d2ef7015
Nikolya <75427338+korhenon@users.noreply.github.com>
2024-04-03 16:02:45 +0300

    Добавлены методы для акций

--- controllers/rates.py
+++ controllers/rates.py
@@ -7,12 +7,12 @@ from common.cb_format import cb_format, iso_from_cb
 from model.fastapi import Currency, DatedCurrency, CurrencyForPeriod, Metal, DatedMetal, MetalForPeriod, Company, \
     DatedCandle
 
-codes = {'AUD': 'R01010', 'AZN': 'R01020A', 'AMD': 'R01060', 'BYN': 'R01090B', 'BGN': 'R01100', 'BRL': 'R01115',
+codes = {'AUD': 'R01010', 'AZN': 'R01020', 'AMD': 'R01060', 'BYN': 'R01090', 'BGN': 'R01100', 'BRL': 'R01115',
          'HUF': 'R01135', 'VND': 'R01150', 'HKD': 'R01200', 'GEL': 'R01210', 'DKK': 'R01215', 'AED': 'R01230',
          'USD': 'R01235', 'EUR': 'R01239', 'EGP': 'R01240', 'INR': 'R01270', 'IDR': 'R01280', 'KZT': 'R01335',
          'CAD': 'R01350', 'QAR': 'R01355', 'KGS': 'R01370', 'CNY': 'R01375', 'MDL': 'R01500', 'NZD': 'R01530',
-         'NOK': 'R01535', 'PLN': 'R01565', 'RON': 'R01585F', 'XDR': 'R01589', 'SGD': 'R01625', 'TJS': 'R01670',
-         'THB': 'R01675', 'TRY': 'R01700J', 'UZS': 'R01717', 'UAH': 'R01720', 'CZK': 'R01760', 'SEK': 'R01770',
+         'NOK': 'R01535', 'PLN': 'R01565', 'RON': 'R01585', 'XDR': 'R01589', 'SGD': 'R01625', 'TJS': 'R01670',
+         'THB': 'R01675', 'TRY': 'R01700', 'UZS': 'R01717', 'UAH': 'R01720', 'CZK': 'R01760', 'SEK': 'R01770',
          'CHF': 'R01775', 'ZAR': 'R01810', 'JPY': 'R01820', "GBP": "R01035"}
 
 
