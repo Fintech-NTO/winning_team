109ba4ba307a279a1f858921d0d7f49c24bccf90
Nikolya <75427338+korhenon@users.noreply.github.com>
2024-04-03 11:43:32 +0300

    Добавлен CORS

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from routers import rates
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(rates.router)
